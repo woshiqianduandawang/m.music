6d40935e3ffb4b363a48e61f64afaca503faedb7
cc <1174149547@qq.com>
2022-12-30 14:44:56 +0800

    优化跳转

--- src/main.js
+++ src/main.js
@@ -21,7 +21,6 @@ if (
   /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)
 ) {
   // 执行相应代码或直接跳转到手机页面
-  window.location.href = "https://miniccmusic.netlify.app/";
 } else {
   // 执行桌面端代码
   console.log('桌面');

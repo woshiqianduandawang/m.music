f75ffda9fd837eed914f95db2e96a302cd0bc459
cc <1174149547@qq.com>
2023-05-08 00:54:03 +0800

    修改跳转网址

--- src/main.js
+++ src/main.js
@@ -17,12 +17,12 @@ new Vue({
   render: h => h(App),
 
 })
-// if (
-//   /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)
-// ) {
-//   // 执行相应代码或直接跳转到手机页面
-// } else {
-//   // 执行桌面端代码
-//   console.log('桌面');
-//   window.location.href = "https://ccmusic.netlify.app/";
-// }
+if (
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)
+) {
+  // 执行相应代码或直接跳转到手机页面
+} else {
+  // 执行桌面端代码
+  console.log('桌面');
+  window.location.href = "https://xcmusic.netlify.app/";
+}
